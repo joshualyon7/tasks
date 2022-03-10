239c0fcf7b6e5607337bef850636ee7df0cc6c51
Josh Lyon <joshlyon@udel.edu>
2022-03-10 11:51:48 -0500

    DoubleHalf code nearly working I think

--- src/bad-components/DoubleHalf.tsx
+++ src/bad-components/DoubleHalf.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
-import { dhValue, setDhValue } from "./DoubleHalfState";
 
-function Doubler(): JSX.Element {
+interface doubleHalfProps {
+    dhValue: number;
+    setDhValue: (x: number) => void;
+}
+
+function Doubler({ dhValue, setDhValue }: doubleHalfProps): JSX.Element {
     return <Button onClick={() => setDhValue(2 * dhValue)}>Double</Button>;
 }
 
-function Halver(): JSX.Element {
+function Halver({ dhValue, setDhValue }: doubleHalfProps): JSX.Element {
     return <Button onClick={() => setDhValue(0.5 * dhValue)}>Halve</Button>;
 }
 
 export function DoubleHalf(): JSX.Element {
+    const [dhValue, setDhValue] = useState(10);
     return (
         <div>
             <h3>Double Half</h3>
